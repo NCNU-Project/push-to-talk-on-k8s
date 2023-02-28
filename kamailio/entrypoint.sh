b8f0b86e49e25132d77ff50443690e90e383ea4b
jerry <efficacy38@gmail.com>
2023-02-28 23:40:20 +0800

    works for deployment

--- kamailio/entrypoint.sh
+++ kamailio/entrypoint.sh
@@ -15,32 +15,5 @@ export PID_FILE
 mkdir -p /data/kamailio
 touch /data/kamailio/dispatcher.list
 
-: ${CLOUD=""} # One of aws, azure, do, gcp, or empty
-if [ "$CLOUD" != "" ]; then
-   PROVIDER="-provider ${CLOUD}"
-fi
-
-: ${PRIVATE_IPV4="$(netdiscover -field privatev4 ${PROVIDER})"}
-: ${PUBLIC_IPV4="$(netdiscover -field publicv4 ${PROVIDER})"}
-: ${PUBLIC_HOSTNAME="$(netdiscover -field hostname ${PROVIDER})"}
-
-TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-NS=$(cat /var/run/secrets/kubernetes.io/serviceaccount/namespace)
-CA_CRT="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
-LOADBALANCER_IP=$(curl -H "Authorization: Bearer $TOKEN" --cacert $CA_CRT https://kubernetes/api/v1/namespaces/$NS/services/kamailio | jq -r .status.loadBalancer.ingress[0].ip)
-LOADBALANCER_IP_REGEX_PATTERN=$(echo $LOADBALANCER_IP | sed 's/\./\\\./g')
-
-cat <<ENDHERE >/data/kamailio/local.k
-#!substdef "/k8s_loadbalencer_domain/${LOADBALANCER_IP}/"
-#!define LB_IP "${LOADBALANCER_IP}"
-#!define LOCAL_IP "${PRIVATE_IPV4}"
-#!subst "/SUB_LB_IP/${LOADBALANCER_IP}/g"
-#!subst "/SUB_LOCAL_IP/${PRIVATE_IPV4}/g"
-#!subst "/LOADBALANCER_IP_REGEX_PATTERN_IP/${LOADBALANCER_IP_REGEX_PATTERN}/g"
-alias=k8s_loadbalencer_domain
-listen=udp:${PRIVATE_IPV4}:5060 advertise k8s_loadbalencer_domain:5060
-listen=udp:${PRIVATE_IPV4}:5080
-ENDHERE
-
 # Runs kamaillio, while shipping stderr/stdout to logstash
 exec /usr/sbin/kamailio $KAMAILIO_ARGS $*
